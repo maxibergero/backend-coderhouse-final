528baad77ac0ecec1f7bfae928bb2ce49e18e0c3
Christian Bergero <christian.javier.bergero@gmail.com>
2024-02-01 12:04:24 -0300

    Se eliminan últimos módulos de prueba para despliegue

--- src/routes/index.routes.js
+++ src/routes/index.routes.js
@@ -4,8 +4,8 @@ import productRouter from "./products.routes.js";
 import sessionRouter from "./session.routes.js";
 import userRouter from "./users.routes.js";
 import ticketRouter from "./tickets.routes.js";
-import mockingRouter from "./mocking.routes.js";
-import loggerRouter from "./logger.routes.js";
+
+
 import envioCorreoRouter from "./envioCorreo.routes.js";
 
 
@@ -17,8 +17,8 @@ router.use('/api/products', productRouter)
 router.use('/api/carts', cartRouter)
 router.use('/api/sessions', sessionRouter)
 router.use('/api/tickets', ticketRouter)
-router.use('/mockingproducts', mockingRouter)
-router.use('/loggerTest', loggerRouter)
+
+
 router.use('/envioCorreo', envioCorreoRouter)
 
 export default router
